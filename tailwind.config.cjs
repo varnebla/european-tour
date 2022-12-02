abe148768bdc1ee7aa2ff3fa96c419e0e22431bf
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2022-12-02 17:23:05 +0100

    feat: add first day section, add when we leaving

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        background: '#FAF5E4',
+        primary: '#125B50',
+        secondary: '#FF6363',
+        tertiary: '#F8B400'
+      }
+    },
+    fontFamily: {
+      'dm-serif': ['DM Serif Display'],
+      'dm-sans': ['DM Sans']
+    }
+  },
+  plugins: []
+};
